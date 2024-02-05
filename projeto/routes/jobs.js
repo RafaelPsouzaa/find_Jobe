eefde2c4e836bea8ef1d72870e816255364afdba
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2024-02-05 16:18:36 -0300

    02:45

--- projeto/routes/jobs.js
+++ projeto/routes/jobs.js
@@ -7,6 +7,17 @@ router.get('/test', (req, res) => {
     res.send('deu certo');
 });
 
+// detalhe da vaga 
+router.get('/view/:id',(req,res) =>Job.findOne({
+    where:{id: req.params.id}
+}).then(job =>{
+    res.render('view',{
+        job
+    });
+}).catch(err => console.log(err));
+) 
+
+
 // recebendo dados do formulario add
 router.get('/add',(req,res)=>{
     res.render('add');
